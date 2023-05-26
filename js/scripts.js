210984278fbac3325e042a41326b1dbe847ef2b3
TheRealZachTV <84297339+TheRealZachTV@users.noreply.github.com>
2023-05-26 19:05:14 -0400

    Updated Scripts.js
    Added Pokemon to test the listing system

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-alert('Hello World!');
-let favoriteFood = 'Coconut';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Growlithe', height: 0.6, type: ['Fire']},
+    { name: 'Arcanine', height: 1.9, type: ['Fire']},
+    { name: 'Totodile', height: 0.6, type: ['Water']},
+    { name: 'Croconaw', height: 1.1, type: ['Water']},
+    { name: 'Feraligatr', height: 2.3, type: ['Water']},
+    { name: 'Poliwrath', height: 1.3, type: ['Water', 'Fighting']}
+    
+];
+
+console.log(pokemonList[5].type)
